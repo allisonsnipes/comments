0034478c6dc630f2a7e85f7d6eca955084367965
Allison Snipes <ajsnipes@usfca.edu>
2021-08-21 11:40:03 -0700

    finished with the lab

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+"""This code is used to follow along with lab 1-2"""
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+# this uses the function above to display the name in the print statement above
+if __name__ == '__main__':
+    print_hi('allison')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
